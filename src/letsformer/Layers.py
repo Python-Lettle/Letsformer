f7c607ec83fb65a0b9bb9b542cb0ccf50cd67263
Python-Lettle <1071445082@qq.com>
2026-05-04 19:21:10 +0800

    RMSNorm & PWFFN

--- src/letsformer/Layers.py
+++ src/letsformer/Layers.py
@@ -75,6 +75,68 @@ class Embedding(nn.Module):
         '''
         return self.weight[token_ids]
 
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        '''
+            Construct the RMSNorm module.
+            接收参数：
+                d_model: int Hidden dimension of the model
+                eps: float = 1e-5 Epsilon value for numerical stability
+                device: torch.device | None = None Device to store the parameters on
+                dtype: torch.dtype | None = None Data type of the parameters
+        '''
+        super().__init__()
+        # gaim parameter
+        self.weights = nn.Parameter(torch.ones(d_model, device=device))
+        self.eps = eps
 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+            处理输入形状为 (batch_size, sequence_length, d_model) 的输入 x，并且返回一个相同形状的 Tensor
+        '''
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        
+        rms: Tensor = torch.sqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+        result = x * self.weights / rms
+        
+        return result.to(in_dtype)
+
+class PWFFN(nn.Module):
+    '''
+        PWFFN --- Position-Wise Feed-Forward Network
+        由 SiLU 激活函数和 GLU 组成的 SwiGLU 前馈网络
+    '''
+    def __init__(self, d_ff: int, d_model: int, device=None, weights: list[Tensor] | None = None):
+        super().__init__()
+
+        if weights:
+            self.W1: Float[Tensor, " d_ff d_model"] = nn.Parameter(
+                weights[0], requires_grad=True)
+            self.W2: Float[Tensor, " d_model d_ff"] = nn.Parameter(
+                weights[1], requires_grad=True)
+            self.W3: Float[Tensor, " d_ff d_model"] = nn.Parameter(
+                weights[2], requires_grad=True)
+        else:
+            self.W1: Float[Tensor, " d_ff d_model"] = nn.Parameter(
+                torch.empty(d_ff, d_model, device=device), requires_grad=True)
+            self.W2: Float[Tensor, " d_model d_ff"] = nn.Parameter(
+                torch.empty(d_model, d_ff, device=device), requires_grad=True)
+            self.W3: Float[Tensor, " d_ff d_model"] = nn.Parameter(
+                torch.empty(d_ff, d_model, device=device), requires_grad=True)
+    
+    def forward(self, x: Float[Tensor, " ... d_model"]) -> Float[Tensor, " ... d_model"]:
+        '''
+            FFN(x) = SwiGLU(x, w1, w2, w3) = w2( SiLU(w1 * x) ⊙ w3 * x) )
+        '''
+        w1x = einsum(self.W1, x, "... d_ff d_model, ... d_model -> ... d_ff")    # Shape: [... d_ff]
+        w3x = einsum(self.W3, x, "... d_ff d_model, ... d_model -> ... d_ff")    # Shape: [... d_ff]
+        silu_result = silu(w1x)                         # SiLU(w1 * x)  Shape: [... d_ff]
+        # FFNx = self.W2 @ (silu.mul(w3x))              # 
+        FFNx = einsum(self.W2, silu_result.mul(w3x), "... d_model d_ff, ... d_ff -> ... d_model")
+
+        return FFNx
+        
 
 
